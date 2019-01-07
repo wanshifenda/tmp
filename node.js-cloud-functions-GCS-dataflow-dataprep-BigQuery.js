e16daff8c04a039991300eacdba35d1e9594d53e
Terence <terenceheipi@gmail.com>
2019-01-07 19:01:41 +0000

    BQ

---
+++ node.js-cloud-functions-GCS-dataflow-dataprep-BigQuery.js
@@ -0,0 +1,50 @@
+const google = require('googleapis'); 
+
+exports.runDataprep = (event, callback) => {
+  const file = event.data;
+  var i = file.name.lastIndexOf('.');
+  const file_ext = (i < 0) ? '' : file.name.substr(i);
+  console.log(`Detecting new file uploaded: ${file.bucket}/${file.name}`);
+
+  if(file_ext == '.gz' && file.name.indexOf('Chartbeat') > -1){
+    google.auth.getApplicationDefault(function (err, authClient) {
+      if (err) {
+        throw err;
+      }
+
+      if (authClient.createScopedRequired && authClient.createScopedRequired()) {
+        authClient = authClient.createScoped([
+          "https://www.googleapis.com/auth/cloud-platform",
+          "https://www.googleapis.com/auth/userinfo.email"
+        ]); 
+      }
+
+      google.auth.getDefaultProjectId(function(err, projectId) {
+        if (err || !projectId) {
+          console.error(`Problems getting projectId (${projectId}). Err was: `, err);
+          throw err;
+        }
+
+        const dataflow = google.dataflow({ version: "v1b3", auth: authClient });
+
+        dataflow.projects.templates.create({
+          "projectId": projectId,
+          "resource": {
+            "gcsPath": "gs://dev-staging-bucket/terence.shi@bbc.co.uk/temp/cloud-dataprep-chartbeat-test-1392317-by-terenceshi_template",
+            "jobName": "Test-prep-Gfunctions-flow-to-BQ",
+            "parameters": {
+              "customGcsTempLocation": "gs://dev-staging-bucket/terence.shi@bbc.co.uk/temp",
+              "outputLocations": "{\"location1\": \"bbc-data-warehouse-dev-7214:chartbeat.sessions\"}",
+              "inputLocations": `{"location1": "gs://${file.bucket}/${file.name}"}`
+            },
+            "environment": {
+              "tempLocation": "gs://dev-staging-bucket/temp/dataflow-cloud-functions-dataprep"
+            }
+          }
+        });
+      });
+    });
+  } else {
+    console.error(`Err was: wrong ChartBeat file (${file.name})`);
+  }
+};
